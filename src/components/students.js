22fe63c97d6dc72576b4acdb6f04de3271cdd8f6
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-05-18 23:42:22 -0700

    handlers

--- src/components/students.js
+++ src/components/students.js
@@ -4,10 +4,36 @@ import StudentList from './studentList';
 
 export default function Students() {
   const [students, setStudents] = useState(data)
+  const [name, setName] = useState("")
+  const [rollno, setRollno] = useState("")
+  const [stdclass, stdClass] = useState("")
+
+  // const innputHandler = () => {
+  //   console.log("e", e.target.name);
+  //   console.log("E", e.target.value);
+  // }
+
+  const ctaHandler = () => {
+    let student = {
+      name,
+      rollno,
+      stdclass,
+    }
+    console.log(student);
+    // new data add in student list
+  }
   return (
     <div className="container">
       <div className="row">
         <div className="col">
+          <input type="text" placeholder='name' name='name' onChange={(e) => setName(e.target.value)} />
+          <input type="text" placeholder='RollNo' name='name' onChange={(e) => setRollno(e.target.value)} />
+          <input type="text" placeholder='Class' onChange={(e) => stdClass(e.target.value)} />
+          <button onClick={ctaHandler}>
+            Submit
+          </button>
+          <hr />
+          <h1>List of Students</h1>
           <table class="table">
             <thead>
               <tr>
